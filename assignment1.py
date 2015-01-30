f169932748ecb6235209514e21164c88f8d4f11c
Omar Khalid <omar.khalid.yahya@gmail.com>
2015-01-30 17:11:20 +0800

    Finished assignment 1. Need to submit.

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,13 @@
 from sys import argv
 from scipy.io import wavfile
 import numpy
+import csv
 
 script, filename = argv
 
 txt = open(filename)
 fileList = txt.readlines()
-result = []
+b = open('assignment1.csv', 'wb')
 for i in fileList:
 	j, k = i.split("\t") #split string after \t
 	rate, sample = wavfile.read(j) #read in wavfile
@@ -14,8 +15,15 @@ for i in fileList:
 	npArray = numpy.array(sample)
 	len = numpy.size(npArray)
 	RMS = numpy.sqrt((1/ float(len))*(numpy.sum(numpy.square(npArray))))
-	result.append(RMS)
-	result.append((numpy.argmax(numpy.abs(npArray)))/RMS) #calculate PAR
-	result.append((1/(len-1))(#something))#calculate ZCR
-	result.append(numpy.median(numpy.abs(npArray - numpy.median(npArray))))#calculate MAD
+	PAR = (numpy.max(numpy.absolute(npArray)))/RMS
+	npSign = numpy.sign(npArray)
+	npBinary = npSign[1:] * npSign[:-1]
+	npNeg = numpy.array([x for x in npBinary if x < 0])
+	npNeg[npNeg < 0] = 1
+	ZCR = (1/float(len-1))*(numpy.sum(npNeg))
+	MAD = numpy.median(numpy.abs(npArray - numpy.median(npArray)))
 	
+	a = csv.writer(b)
+	data = [j, RMS, PAR, ZCR, MAD]
+	a.writerow(data)
+b.close()
