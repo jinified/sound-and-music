d70598ab2716bfcf999c15e9679cc6baa2e5f299
Omar Khalid <omar.khalid.yahya@gmail.com>
2015-01-30 15:33:16 +0800

    New edits to Assignment 1. Finished 1st formula. 2nd formula untested. Third formula incomplete. Fourth formula untested.

--- ground_truth.py
+++ ground_truth.py
@@ -1,12 +1,21 @@
 from sys import argv
 from scipy.io import wavfile
+import numpy
 
 script, filename = argv
 
 txt = open(filename)
 fileList = txt.readlines()
+result = []
 for i in fileList:
-	j, k = i.split("\t")
-	rate, sample = wavfile.read(j)
-	print sample
+	j, k = i.split("\t") #split string after \t
+	rate, sample = wavfile.read(j) #read in wavfile
+	sample = sample / 32768.0 #convert sample to floats
+	npArray = numpy.array(sample)
+	len = numpy.size(npArray)
+	RMS = numpy.sqrt((1/ float(len))*(numpy.sum(numpy.square(npArray))))
+	result.append(RMS)
+	result.append((numpy.argmax(numpy.abs(npArray)))/RMS) #calculate PAR
+	result.append((1/(len-1))(#something))#calculate ZCR
+	result.append(numpy.median(numpy.abs(npArray - numpy.median(npArray))))#calculate MAD
 	
