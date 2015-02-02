685d837f02b5c78d99e17efe40b5c97d48204c38
Omar Khalid <omar.khalid.yahya@gmail.com>
2015-02-02 19:56:15 +0800

    Code finished. Images generated. ARFF correct. Left with weka algorithm analysis.

--- Assignment 2/assignment2.py
+++ Assignment 2/assignment2.py
@@ -27,17 +27,21 @@ for i in range(num_files):
 	j, k = fileList[i].split("\t") #split string after \t
 	rate, sample = wavfile.read(j) #read in wavfile
 	sample = sample / 32768.0 #convert sample to floats
-	npArray = np.array(sample)
-	RMS = np.sqrt((1/ float(num_files))*(np.sum(np.square(npArray))))
-	PAR = (np.max(np.absolute(npArray)))/RMS
-	npSign = np.sign(npArray)
+	sampleArray = np.array(sample) #creates array of sample values
+	len = np.size(sampleArray) #number of samples of all files
+	RMS = np.sqrt((1/ float(len))*(np.sum(np.square(sampleArray))))
+	PAR = (np.max(np.absolute(sampleArray)))/RMS
+	
+	#to calculate ZCR
+	npSign = np.sign(sampleArray)
 	npBinary = npSign[1:] * npSign[:-1]
 	npNeg = np.array([x for x in npBinary if x < 0])
 	npNeg[npNeg < 0] = 1
-	ZCR = (1/float(num_files-1))*(np.sum(npNeg))
-	MAD = np.median(np.abs(npArray - np.median(npArray)))
+	ZCR = (1/float(len-1))*(np.sum(npNeg))
+
+	MAD = np.median(np.abs(sampleArray - np.median(sampleArray)))
 	
-	f.write("%f,%f,%f,%f,%s" %(RMS, PAR, ZCR, MAD, k))
+	f.write("%f,%f,%f,%f,%s" %(RMS, PAR, ZCR, MAD, k)) #write to arff file
 	
 	data = [RMS, PAR, ZCR, MAD]
 	if "music" in k:
@@ -45,9 +49,20 @@ for i in range(num_files):
 	else:
 		features_speech[i] = data
 
-plt.plot(features_music[:,2], features_music[:,1])
-plt.savefig("music.png")
-plt.plot(features_speech[:,2], features_speech[:,1])
-plt.savefig("speech.png")
+ZCRplot = plt.scatter(features_music[:,2], features_music[:,1], c = "red", marker = "o")
+PARplot = plt.scatter(features_speech[:,2], features_speech[:,1], c = "blue", marker = "x")
+plt.xlabel("ZCR")
+plt.ylabel("PAR")
+plt.legend((ZCRplot, PARplot), ('ZCR', 'PAR'), loc = 4)
+plt.savefig("ZCRxPAR.png")
+
+plt.clf() #clear the figure
+
+RMSplot = plt.scatter(features_music[:,0], features_music[:,3], c = "green", marker = "^")
+MADplot = plt.scatter(features_speech[:,0], features_speech[:,3], c = "orange", marker = "s")
+plt.xlabel("RMS")
+plt.ylabel("MAD")
+plt.legend((RMSplot, MADplot), ('RMS', 'MAD'), loc = 4)
+plt.savefig("RMSxMAD.png")
 
 f.close()
