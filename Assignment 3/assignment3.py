dfe177aa1d5b661139bb07c22fe7af9f93048193
Omar Khalid <omar.khalid.yahya@gmail.com>
2015-02-22 14:49:47 +0800

    Completed A3.

--- Assignment 3/assignment3.py
+++ Assignment 3/assignment3.py
@@ -8,6 +8,7 @@ script, filename = argv
 txt = open(filename)
 fileList = txt.readlines()
 num_files = len(fileList)
+bufferMatrix = np.zeros((1290, 1024))
 
 f = open("whole-song.arff", "w")
 f.write('''@RELATION music_speech
@@ -38,8 +39,8 @@ for i in range(num_files):
 	rate, sample = wavfile.read(j) #read in wavfile
 	sample = sample / 32768.0 #convert sample to floats
 	sampleArray = np.array(sample) #creates array of sample values
-	len = np.size(sampleArray) #number of samples of all files
-	RMS = np.sqrt((1/ float(len))*(np.sum(np.square(sampleArray))))
+	length = np.size(sampleArray) #number of samples of all files
+	RMS = np.sqrt((1/ float(length))*(np.sum(np.square(sampleArray))))
 	PAR = (np.max(np.absolute(sampleArray)))/RMS
 	
 	#to calculate ZCR
@@ -47,52 +48,83 @@ for i in range(num_files):
 	npBinary = npSign[1:] * npSign[:-1]
 	npNeg = np.array([x for x in npBinary if x < 0])
 	npNeg[npNeg < 0] = 1
-	ZCR = (1/float(len-1))*(np.sum(npNeg))
+	ZCR = (1/float(length-1))*(np.sum(npNeg))
 
 	MAD = np.median(np.abs(sampleArray - np.median(sampleArray)))
 	f.write("%f,%f,%f,%f,%s" %(RMS, PAR, ZCR, MAD, k)) #write to original arff file
 
-	RMS_data = np.empty_like(sampleArray)
-	PAR_data = np.empty_like(sampleArray)
-	ZCR_data = np.empty_like(sampleArray)
-	MAD_data = np.empty_like(sampleArray)
-	MeanAD_data = np.empty_like(sampleArray)
+	buffer_data = []
+	num_buffer = int(length/1024) * 2
+	start = 0
+	end = 1024
+	for j in range(num_buffer):
+		buffer_data = sampleArray[start:end]
+		bufferMatrix[j,: ] = buffer_data
+		start = start + 512
+		end = end + 512
+		
+	featureMatrix = np.zeros((num_buffer, 5))
 	
-	for i in range(0, len/512):
-		start = i * 512
-		end = (i+2) * 512
-		buffer_data = sample[start:end]
-		
-		RMS = np.sqrt(np.mean(np.square(buffer_data)))
-		PAR = (np.max(np.absolute(buffer_data)))/RMS
-		
-		#to calculate ZCR
-		npSign = np.sign(buffer_data)
-		npBinary = npSign[1:] * npSign[:-1]
-		npNeg = np.array([x for x in npBinary if x < 0])
-		npNeg[npNeg < 0] = 1
-		ZCR = (1/1023.0)*(np.sum(npNeg))
-		
-		MAD = np.median(np.abs(buffer_data - np.median(buffer_data)))
-		MEAN_AD = np.mean(np.abs(buffer_data - np.mean(buffer_data)))
-		
-		np.append(RMS_data, RMS)
-		np.append(PAR_data, PAR)
-		np.append(ZCR_data, ZCR)
-		np.append(MAD_data, MAD)
-		np.append(MeanAD_data, MEAN_AD)
+	#to calculate RMS
+	#RMS = np.sqrt(np.mean(np.square(bufferMatrix)))
+	RMSMatrix = np.copy(bufferMatrix)
+	RMSMatrix = np.power(RMSMatrix, 2)
+	RMS = np.sum(RMSMatrix, axis = 1)
+	RMS = RMS/1024
+	RMS = np.sqrt(RMS)
 	
-	RMS_MEAN = np.mean(RMS_data)
-	PAR_MEAN = np.mean(PAR_data)
-	ZCR_MEAN = np.mean(ZCR_data)
-	MAD_MEAN = np.mean(MAD_data)
-	MEAN_AD_MEAN = np.mean(MeanAD_data)
+	#to calculate PAR
+	#PAR = (np.max(np.absolute(bufferMatrix)))/RMS
+	PARMatrix = np.copy(bufferMatrix)
+	PARMatrix = np.absolute(PARMatrix)
+	PAR = np.max(PARMatrix, axis = 1)
+	PAR = PAR/RMS
+		
+	#to calculate ZCR
+	ZCRMatrix = np.copy(bufferMatrix)
+	ZCRa = ZCRMatrix[:1290, :1023]
+	ZCRb = ZCRMatrix[:1290, 1:1024]
+	ZCRc = ZCRa * ZCRb
+	ZCR = np.where(ZCRc < 0, 1, 0)
+	ZCR = np.sum(ZCR, axis = 1)
+	ZCR = ZCR/1023.0
+
+	#to calculate MAD
+	#MAD = np.median(np.abs(buffer_data - np.median(bufferMatrix)))
+	MADMatrix = np.copy(bufferMatrix)
+	firstMed = np.median(MADMatrix, axis = 1)
+	firstMed = np.reshape(firstMed, (len(firstMed), 1))
+	MADMatrix = np.absolute(MADMatrix - firstMed)
+	MAD = np.median(MADMatrix, axis = 1)
 	
-	RMS_STD = np.std(RMS_data)
-	PAR_STD = np.std(PAR_data)
-	ZCR_STD = np.std(ZCR_data)
-	MAD_STD = np.std(MAD_data)
-	MEAN_AD_STD = np.std(MeanAD_data)
+	#to calculate MEAN_AD
+	#MEAN_AD = np.mean(np.abs(buffer_data - np.mean(bufferMatrix)))
+	MEANMatrix = np.copy(bufferMatrix)
+	firstMean = np.mean(MEANMatrix, axis = 1)
+	firstMean = np.reshape(firstMean, (len(firstMean), 1))
+	MEANMatrix = np.absolute(MEANMatrix - firstMean)
+	MEAN_AD = np.mean(MEANMatrix, axis = 1)
+	
+	featureMatrix[:,0] = RMS
+	featureMatrix[:,1] = PAR
+	featureMatrix[:,2] = ZCR
+	featureMatrix[:,3] = MAD
+	featureMatrix[:,4] = MEAN_AD
+	
+	writeMatrix = np.zeros(10)
+	writeMatrix[0:5] = np.mean(featureMatrix, axis = 0)
+	writeMatrix[5:10] = np.std(featureMatrix, axis = 0)
+	RMS_MEAN = writeMatrix[0]
+	PAR_MEAN = writeMatrix[1]
+	ZCR_MEAN = writeMatrix[2]
+	MAD_MEAN = writeMatrix[3]
+	MEAN_AD_MEAN = writeMatrix[4]
+	
+	RMS_STD = writeMatrix[5]
+	PAR_STD = writeMatrix[6]
+	ZCR_STD = writeMatrix[7]
+	MAD_STD = writeMatrix[8]
+	MEAN_AD_STD = writeMatrix[9]
 	
 	g.write("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%s" %(RMS_MEAN, PAR_MEAN, ZCR_MEAN, MAD_MEAN, MEAN_AD_MEAN, RMS_STD, PAR_STD, ZCR_STD, MAD_STD, MEAN_AD_STD, k)) #write to new arff file
 	
